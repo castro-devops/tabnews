a4e7339288dc0bfd80047a2b5e94759af529471d
castro-devops <breno.castro.ofc@gmail.com>
2024-11-14 19:38:16 +0000

    Commit inicial do Next.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+
+    return <h5>Oi meu neném, essa página é dedicada a você, que para além de desenvolver serviços web, que isso cause impacto nas pessoas que o usarem, então o primeiro impacto teria que ser em você, saiba que sou grato e feliz por todo o seu apoio e por acreditar até mesmo quando eu mesmo não creio, por ser a base de força, fé e carinho, e saiba que ainda temos muita história pra construirmos, e que cada dificuldade, cada desafio, cada superação, vai ser um parágrafo a mais nesse final feliz, lhe amo grandemente. De Breno Castro para Vanessa Dantas</h5>
+
+}
+
+export default Home;
